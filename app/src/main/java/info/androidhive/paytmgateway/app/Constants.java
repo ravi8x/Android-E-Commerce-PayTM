e873199e7d191d176b6e56111cfbe84bb2a4a169
Ravi Tamada <ravi8x@gmail.com>
2018-12-25 12:53:42 +0530

    Added method to add cart item

--- app/src/main/java/info/androidhive/paytmgateway/app/Constants.java
+++ app/src/main/java/info/androidhive/paytmgateway/app/Constants.java
@@ -2,5 +2,5 @@ package info.androidhive.paytmgateway.app;
 
 public class Constants {
     public static final String DB_NAME = "paytm.androidhive";
-    public static final String BASE_URL = "https://887d8286.ngrok.io/api/";
+    public static final String BASE_URL = "https://5b045b31.ngrok.io/api/";
 }
--- app/src/main/java/info/androidhive/paytmgateway/db/AppDatabase.java
+++ app/src/main/java/info/androidhive/paytmgateway/db/AppDatabase.java
@@ -1,5 +1,6 @@
 package info.androidhive.paytmgateway.db;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +9,9 @@ import info.androidhive.paytmgateway.db.model.CartItem;
 import info.androidhive.paytmgateway.networking.model.AppConfig;
 import info.androidhive.paytmgateway.networking.model.Product;
 import io.realm.Realm;
+import io.realm.RealmList;
 import io.realm.RealmResults;
+import timber.log.Timber;
 
 public class AppDatabase {
     public AppDatabase() {
@@ -31,23 +34,47 @@ public class AppDatabase {
     }
 
     public static void addItemToCart(Product product) {
+        Timber.e("addItemToCart: %d", product.id);
         Cart cart = getCart();
-        Realm realm = Realm.getDefaultInstance();
 
         if (cart == null) {
-            realm.executeTransactionAsync(realm1 -> {
+            Timber.e("cart is null. Creating new one");
+            Realm.getDefaultInstance().executeTransaction(realm -> {
                 List<CartItem> cartItems;
-                Cart cart1 = Realm.getDefaultInstance().createObject(Cart.class, 0);
+                Cart cart1 = realm.createObject(Cart.class, 0);
                 CartItem cartItem = new CartItem();
                 cartItem.product = product;
                 cartItem.quantity += 1;
                 cartItems = new ArrayList<>();
                 cartItems.add(cartItem);
+                cart1.cartItems.addAll(cartItems);
 
-                realm1.copyToRealmOrUpdate(cart1);
+                Timber.e("Cart1: %s", cart1);
+
+                realm.copyToRealmOrUpdate(cart1);
             });
         } else {
             // existed cart is present
+            Timber.e("cart items size: %d", cart.cartItems.size());
+            CartItem item = cart.cartItems.where().equalTo("product.id", product.id).findFirst();
+            if (item != null) {
+                Timber.e("cart item found: %s", item);
+                Realm.getDefaultInstance().executeTransaction(realm -> {
+                    item.quantity += 1;
+                    cart.cartItems.set(cart.cartItems.indexOf(item), item);
+                    realm.copyToRealmOrUpdate(cart);
+                });
+
+            } else {
+                Timber.e("cart item null. Creating new one");
+                CartItem cartItem = new CartItem();
+                cartItem.product = product;
+                cartItem.quantity += 1;
+                Realm.getDefaultInstance().executeTransaction(realm -> {
+                    cart.cartItems.add(cartItem);
+                    realm.copyToRealmOrUpdate(cart);
+                });
+            }
         }
     }
 
